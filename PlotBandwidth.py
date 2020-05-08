af9ab0ec1f51826b6a528139314171907c55a15b
anders.rylander2 <arylander@gmail.com>
2020-05-08 13:29:20 +0100

    Removal of non relevant files.

--- PlotBandwidth.py
+++
@@ -1,7 +0,0 @@
-from pandas import read_csv
-from matplotlib import pyplot
-series = read_csv('bandwidth_20200309_0100_81.csv', header=0, index_col=0, parse_dates=True, squeeze=True)
-#print(series.head())
-series.plot()
-#pyplot.show()
-pyplot.savefig('Bandwidth.png')
